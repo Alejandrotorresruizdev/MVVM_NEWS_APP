211bd36e0071ca248a02bea83f24e1a2cdc6d3c4
Alejandro Torres <alejandrojuegoselx@gmail.com>
2020-09-23 16:31:52 +0200

    [ADD] Created retrofit instance

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY ="3a2c39b7ab344eb0aa64c949a5805bd6"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
