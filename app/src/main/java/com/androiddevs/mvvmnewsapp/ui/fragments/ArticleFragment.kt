77692b982f088ce1e174cf484ec62937e3350c18
Alejandro Torres <alejandrojuegoselx@gmail.com>
2020-09-24 18:07:59 +0200

    [ADD] Finished creation and elimination from room

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/ArticleFragment.kt
@@ -8,6 +8,7 @@ import androidx.navigation.fragment.navArgs
 import com.androiddevs.mvvmnewsapp.R
 import com.androiddevs.mvvmnewsapp.ui.NewsActivity
 import com.androiddevs.mvvmnewsapp.ui.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_article.*
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
@@ -19,9 +20,15 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
         val article = args.article
+
         webView.apply {
             webViewClient = WebViewClient()
             loadUrl(article.url)
+        }
+
+        fab.setOnClickListener{
+            viewModel.saveArticle(article)
+            Snackbar.make(view,"Article save succesfully",Snackbar.LENGTH_SHORT).show()
         }
     }
 }
